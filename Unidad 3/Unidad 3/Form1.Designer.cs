9ce69a8fbd9774e1b91d373e057cc3775302c89a
AlejandroVelezGuillermo <125221168+AlejandroVelezGuillermo@users.noreply.github.com>
2023-09-21 17:11:13 -0600

    Add files via upload

---
+++ Unidad 3/Unidad 3/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Unidad_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPush = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnPop = new System.Windows.Forms.Button();
+            this.btnEnqueue = new System.Windows.Forms.Button();
+            this.btnDequeue = new System.Windows.Forms.Button();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.txtPila = new System.Windows.Forms.TextBox();
+            this.txtCola = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnPush
+            // 
+            this.btnPush.Location = new System.Drawing.Point(0, 0);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(75, 23);
+            this.btnPush.TabIndex = 0;
+            this.btnPush.Text = "button1";
+            this.btnPush.UseVisualStyleBackColor = true;
+            this.btnPush.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(599, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnPop
+            // 
+            this.btnPop.Location = new System.Drawing.Point(121, 48);
+            this.btnPop.Name = "btnPop";
+            this.btnPop.Size = new System.Drawing.Size(75, 23);
+            this.btnPop.TabIndex = 2;
+            this.btnPop.Text = "button2";
+            this.btnPop.UseVisualStyleBackColor = true;
+            this.btnPop.Click += new System.EventHandler(this.btnPop_Click);
+            // 
+            // btnEnqueue
+            // 
+            this.btnEnqueue.Location = new System.Drawing.Point(277, 47);
+            this.btnEnqueue.Name = "btnEnqueue";
+            this.btnEnqueue.Size = new System.Drawing.Size(75, 23);
+            this.btnEnqueue.TabIndex = 3;
+            this.btnEnqueue.Text = "button2";
+            this.btnEnqueue.UseVisualStyleBackColor = true;
+            this.btnEnqueue.Click += new System.EventHandler(this.btnEnqueue_Click);
+            // 
+            // btnDequeue
+            // 
+            this.btnDequeue.Location = new System.Drawing.Point(376, 47);
+            this.btnDequeue.Name = "btnDequeue";
+            this.btnDequeue.Size = new System.Drawing.Size(75, 23);
+            this.btnDequeue.TabIndex = 4;
+            this.btnDequeue.Text = "button3";
+            this.btnDequeue.UseVisualStyleBackColor = true;
+            this.btnDequeue.Click += new System.EventHandler(this.btnDequeue_Click);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(76, 164);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 22);
+            this.txtValor.TabIndex = 5;
+            // 
+            // txtPila
+            // 
+            this.txtPila.Location = new System.Drawing.Point(249, 164);
+            this.txtPila.Name = "txtPila";
+            this.txtPila.Size = new System.Drawing.Size(100, 22);
+            this.txtPila.TabIndex = 6;
+            // 
+            // txtCola
+            // 
+            this.txtCola.Location = new System.Drawing.Point(416, 164);
+            this.txtCola.Name = "txtCola";
+            this.txtCola.Size = new System.Drawing.Size(100, 22);
+            this.txtCola.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtCola);
+            this.Controls.Add(this.txtPila);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.btnDequeue);
+            this.Controls.Add(this.btnEnqueue);
+            this.Controls.Add(this.btnPop);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnPush);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPush;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPop;
+        private System.Windows.Forms.Button btnEnqueue;
+        private System.Windows.Forms.Button btnDequeue;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtPila;
+        private System.Windows.Forms.TextBox txtCola;
+    }
+}
+
---
+++ Unidad 3/Unidad 3/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unidad_3
+{
+    public partial class Form1 : Form
+    {
+        private int[] pila = new int[10]; // Arreglo para la pila
+        private int[] cola = new int[10]; // Arreglo para la cola
+        private int topePila = -1; // Inicializar el tope de la pila
+        private int frenteCola = 0; // Inicializar el frente de la cola
+        private int finCola = -1; // Inicializar el final de la cola
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Método para agregar un elemento a la pila
+        private void PushToPila(int valor)
+        {
+            if (topePila < pila.Length - 1)
+            {
+                pila[++topePila] = valor;
+            }
+            else
+            {
+                MessageBox.Show("La pila está llena. No se puede agregar más.");
+            }
+        }
+        // Método para quitar un elemento de la pila
+        private int PopFromPila()
+        {
+            if (topePila >= 0)
+            {
+                return pila[topePila--];
+            }
+            else
+            {
+                MessageBox.Show("La pila está vacía.");
+                return -1; // Valor por defecto en caso de error
+            }
+        }
+        // Método para agregar un elemento a la cola
+        private void EnqueueToCola(int valor)
+        {
+            if (finCola < cola.Length - 1)
+            {
+                cola[++finCola] = valor;
+            }
+            else
+            {
+                MessageBox.Show("La cola está llena. No se puede encolar más.");
+            }
+        }
+        // Método para quitar un elemento de la cola
+        private int DequeueFromCola()
+        {
+            if (frenteCola <= finCola)
+            {
+                return cola[frenteCola++];
+            }
+            else
+            {
+                MessageBox.Show("La cola está vacía.");
+                return -1; // Valor por defecto en caso de error
+            }
+        }
+
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            int valor = Convert.ToInt32(txtValor.Text);
+            PushToPila(valor);
+            MostrarPila();
+        }
+
+        private void btnPop_Click(object sender, EventArgs e)
+        {
+            int valor = PopFromPila();
+            if (valor != -1)
+            {
+                MessageBox.Show("Elemento retirado de la pila: " + valor);
+            }
+            MostrarPila();
+        }
+
+        private void btnEnqueue_Click(object sender, EventArgs e)
+        {
+            int valor = Convert.ToInt32(txtValor.Text);
+            EnqueueToCola(valor);
+            MostrarCola();
+        }
+
+        private void btnDequeue_Click(object sender, EventArgs e)
+        {
+            int valor = DequeueFromCola();
+            if (valor != -1)
+            {
+                MessageBox.Show("Elemento retirado de la cola: " + valor);
+            }
+            MostrarCola();
+        }
+        private void MostrarPila()
+        {
+            txtPila.Text = string.Join(" -> ", pila, 0, topePila + 1);
+        }
+        private void MostrarCola()
+        {
+            txtCola.Text = string.Join(" -> ", cola, frenteCola, finCola - frenteCola + 1);
+        }
+    }
+}
